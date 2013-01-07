338a308baa9ea945f018868418d1030b09d753ba
Raymond John Hill <raymondjohnhill@gmail.com>
2013-01-07 13:10:24 -0500

    Added LASERS

--- Assets/Beef Invaders/Scripts/ability_move.cs
+++ Assets/Beef Invaders/Scripts/ability_move.cs
@@ -2,14 +2,27 @@ using UnityEngine;
 using System.Collections;
 
 public class ability_move : MonoBehaviour {
-
+	
+	public float speed = 1.0f;
+	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButton("MoveUp")) {
+			transform.position += new Vector3(0.0f, speed * Time.deltaTime, 0.0f);
+		}
+		if(Input.GetButton("MoveDown")) {
+			transform.position += new Vector3(0.0f, -speed * Time.deltaTime, 0.0f);
+		}
+		if(Input.GetButton("MoveLeft")) {
+			transform.position += new Vector3(-speed * Time.deltaTime, 0.0f, 0.0f);
+		}
+		if(Input.GetButton("MoveRight")) {
+			transform.position += new Vector3(speed * Time.deltaTime, 0.0f, 0.0f);
+		}
 	}
 }
--- Assets/Beef Invaders/Scripts/ability_shoot.cs
+++ Assets/Beef Invaders/Scripts/ability_shoot.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public class ability_shoot : MonoBehaviour {
+	
+	public GameObject[] bulletPrefab;
+	public int bulletType = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +13,10 @@ public class ability_shoot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetButton("Shoot")) {
+			Vector3 newPosition = new Vector3(transform.position.x + 0.8f, transform.position.y, transform.position.z);
+			GameObject bulletInstance = (GameObject) Instantiate(this.bulletPrefab[0], newPosition, Quaternion.identity);
+			bulletInstance.rigidbody.AddForce(1.0f * Time.deltaTime, 0.0f, 0.0f);
+		}
 	}
 }
